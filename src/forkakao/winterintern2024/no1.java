897d9d36464e31d1d01db3c04a8bbcea7885cd8b
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-09 16:49:13 +0900

    solution: 2024년 카카오 공채 1번

---
+++ src/forkakao/winterintern2024/no1.java
@@ -0,0 +1,63 @@
+package forkakao.winterintern2024;
+
+import java.util.*;
+
+public class no1 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.solution(new String[]{"joy", "brad", "alessandro", "conan", "david"}, new String[]{"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"});
+        System.out.println(answer);
+    }
+}
+
+class Solution {
+
+    private int l;
+    private Map<String, Integer> map = new HashMap<>();
+
+    public int solution(String[] friends, String[] gifts) {
+        l = friends.length;
+        findIdx(friends);
+        int[][] lastMonthCount = new int[l][l];
+        int[] giftPoint = new int[l];
+        for (String record : gifts) {
+            String[] fromTo = record.split(" ");
+            int from = map.get(fromTo[0]);
+            int to = map.get(fromTo[1]);
+            lastMonthCount[from][to]++;
+            giftPoint[from]++;
+            giftPoint[to]--;
+        }
+
+        int[] thisMonthCount = new int[l];
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (lastMonthCount[i][j] > lastMonthCount[j][i]) {
+                    thisMonthCount[i]++;
+                } else if (lastMonthCount[i][j] < lastMonthCount[j][i]) {
+                    thisMonthCount[j]++;
+                } else {
+                    if (giftPoint[i] > giftPoint[j]) {
+                        thisMonthCount[i]++;
+                    } else if (giftPoint[i] < giftPoint[j]) {
+                        thisMonthCount[j]++;
+                    }
+                }
+            }
+        }
+        int max = Arrays.stream(thisMonthCount).max().orElse(0);
+
+        return max;
+    }
+
+    private void findIdx(String[] friends) {
+        for (int i = 0; i < friends.length; i++) {
+            String key = friends[i];
+            map.put(key, i);
+        }
+    }
+}
