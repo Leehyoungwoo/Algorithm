4b92b5d6fbb70a1d4668eb253329a26aa6042a43
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-23 16:54:46 +0900

    solution: 2023년 카카오 채용 3번
    재귀로 중복순열을 구해서 각 이모티콘에 적용될 수 있는 모든 할인율을 적욜시켜본 후, 가입율, 금액 순으로 우선순위를 두어서 최댓값을 구해봄
    실수로 (100-할인율)*가격 / 100을 안해서 수가 너무 커져서 잘못된 값이 나왔지만 구현 중 실수만 하지 않으면 금방 풀 수 있는 문제

---
+++ src/forkakao/blindrecruitment2023/이모티콘_할인행사.java
@@ -0,0 +1,69 @@
+package forkakao.blindrecruitment2023;
+
+import java.util.*;
+
+public class 이모티콘_할인행사 {
+    public static void main(String[] args) {
+        Solution3 solution = new Solution3();
+        int[][] users = new int[][]{{40, 10000}, {25, 10000}};
+        int[] emoticons = new int[]{7000, 9000};
+        int[] answer = solution.solution(users, emoticons);
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+}
+
+class Solution3 {
+
+    private int[] discountRate = {10, 20, 30, 40};
+    private int maxUser = Integer.MIN_VALUE;
+    private int maxSales = Integer.MIN_VALUE;
+    private int[] comb;
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        comb = new int[emoticons.length];
+        findPer(0, users, emoticons);
+        return new int[]{maxUser, maxSales};
+    }
+
+    private void findPer(int idx, int[][] users, int[] emoticons) {
+        if (idx == comb.length) {
+            int[] usersAndSales = findUser(users, emoticons);
+            if (usersAndSales[0] > maxUser) {
+                maxUser = usersAndSales[0];
+                maxSales = usersAndSales[1];
+            }
+            if (usersAndSales[0] == maxUser) {
+                maxSales = Math.max(usersAndSales[1], maxSales);
+            }
+            return;
+        }
+
+        for (int i = 0; i < discountRate.length; i++) {
+            comb[idx] = i;
+            findPer(idx + 1, users, emoticons);
+        }
+    }
+
+    private int[] findUser(int[][] users, int[] emoticons) {
+        int totalPeople = 0;
+        int totalSales = 0;
+        for (int i = 0; i < users.length; i++) {
+            int buy = 0;
+            int rate = users[i][0];
+            int range = users[i][1];
+            for (int j = 0; j < emoticons.length; j++) {
+                if (discountRate[comb[j]] >= rate) {
+                    buy += (100 - discountRate[comb[j]])  * emoticons[j] / 100;
+                }
+                if (buy >= range) {
+                    buy = 0;
+                    totalPeople++;
+                    break;
+                }
+            }
+            totalSales += buy;
+        }
+
+        return new int[] {totalPeople, totalSales};
+    }
+}
