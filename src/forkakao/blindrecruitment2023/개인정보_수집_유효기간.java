a14a1d73ebeda79d7b7a6b19eac13bf3c4bc7740
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-23 15:56:50 +0900

    solution: 2023년 카카오 채용 1번
    문자열을 파싱해서 월이 바뀌는거 계산, 년이 바뀌는거 계산할 수도 있고
    그냥 DateTimeFormatter이랑 LocalDate에 함수로 파싱하면 쉽게 풀 수 있음

---
+++ src/forkakao/blindrecruitment2023/개인정보_수집_유효기간.java
@@ -0,0 +1,57 @@
+package forkakao.blindrecruitment2023;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class 개인정보_수집_유효기간 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] answer = solution.solution("2022.05.19", new String[]{"A 6", "B 12", "C 3" }, new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C" });
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+}
+
+class Solution {
+
+    Map<String, Integer> policy = new HashMap<>();
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> list = new ArrayList<>();
+        // 약관 유효기간 저장
+        for (int i = 0; i < terms.length; i++) {
+            String[] s = terms[i].split(" ");
+            policy.put(s[0], Integer.parseInt(s[1]));
+        }
+
+        // 계산해보기
+        for (int i = 0; i < privacies.length; i++) {
+            String[] s = privacies[i].split(" ");
+            String startDay = s[0];
+            String type = s[1];
+            if (IsDeadline(today, startDay, type) >= 0) {
+                list.add(i + 1);
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    private int IsDeadline(String today, String startDay, String type) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+        LocalDate todayDate = LocalDate.parse(today, formatter);
+        LocalDate startDate = LocalDate.parse(startDay, formatter);
+
+        int validMonth = policy.get(type);
+
+        LocalDate lastDay = convertToLastDay(startDate, validMonth);
+
+        return todayDate.compareTo(lastDay);
+    }
+
+    private LocalDate convertToLastDay(LocalDate startDay, int validMonth) {
+        return startDay.plusMonths(validMonth);
+    }
+}
