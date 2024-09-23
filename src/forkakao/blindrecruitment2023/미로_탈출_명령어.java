3e4dc0bb91adcb2181fd7fabb3ac560518ad4b6c
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-23 22:16:22 +0900

    solution: 2023년 카카오 채용 6번
    구현 문제
    
    우선 움직이는 방향의 배열을 D,L,R,U 알파벳 순으로 지정해주고,(그리디하게)
    dfs탐색을 하는데 중복이 가능하기 떄문에 visited 배열은 필요없고 써서는 안됨
    그리고 중요한 포인트가 k의 길이가 길기 떄문에 그냥 탐색하면 시간 초과 발생,
    조건을 걸어주어야 하는데
    지금위치에서 거리가 K보다 작거나 같아야하고(당연히)
    이동하는 총거리 K에서 지금 현위치로부터의 거리를 뺐을 때, 짝수일때만 목적지에 도착할 수 있다.
    이게 뭔 말이나면 k까지 4남아서 4번 움직일 수 있는데 도착지점까지 거리가 3이면 무슨 수를 써도 도착지점에서 멈출 수 없다. 2이면 중복이 가능하니까 2로 도착지점에 도착하고 옆 칸으로 갔다가 다시 돌아오면 되지만 k-남은 거리가 홀수면 절대 도착할 수 없다. 따라서 이 조건을 재귀마다 확인해주면 도착할 수 없는 많은 케이스가 바로 return되기 때문에 시간 제한을 통과할 수 있다.

---
+++ src/forkakao/blindrecruitment2023/미로_탈출_명령어.java
@@ -0,0 +1,76 @@
+package forkakao.blindrecruitment2023;
+
+import java.util.*;
+
+public class 미로_탈출_명령어 {
+    public static void main(String[] args) {
+        Solution6 solution = new Solution6();
+        String answer = solution.solution(3, 4, 2, 3, 3, 1, 5);
+        System.out.println(answer.equals("dllrl"));
+    }
+}
+
+class Solution6 {
+
+    // D, L, R, U 순서
+    private final int[][] direction = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+    private char[][] map;
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        map = new char[n + 1][m + 1];
+        map[x][y] = 'S';
+        map[r][c] = 'E';
+        answer = findAnwer(x, y, r, c, k, "");
+        return answer.equals("") ? "impossible" : answer;
+    }
+
+    private String findAnwer(int curR, int curC, int r, int c, int k, String way) {
+        if (curR == r && curC == c) {
+            if (k == 0) {
+                return way;
+            }
+        }
+
+        if (!remainWay(curR, curC, r, c, k)) {
+            return "";
+        }
+
+        for (int i = 0; i < direction.length; i++) {
+            int[] dir = direction[i];
+            int nextR = curR + dir[0];
+            int nextC = curC + dir[1];
+            if (isInRange(nextR, nextC)) {
+                String result = findAnwer(nextR, nextC, r, c, k - 1, way + decideDirection(i));
+                if (!result.equals("")) {
+                    return result;
+                }
+            }
+        }
+        return "";
+    }
+
+    private boolean remainWay(int curR, int curC, int r, int c, int k) {
+        int left = Math.abs(curR - r) + Math.abs(curC - c);
+        return left <= k && (k - left) % 2 == 0;
+    }
+
+    private String decideDirection(int i) {
+        switch (i) {
+            case 0:
+                return "d"; // 아래
+            case 1:
+                return "l"; // 왼쪽
+            case 2:
+                return "r"; // 오른쪽
+            case 3:
+                return "u"; // 위
+            default:
+                return "";
+        }
+    }
+
+    private boolean isInRange(int row, int col) {
+        return 1 <= row && row <= map.length - 1 && 1 <= col && col <= map[0].length - 1;
+    }
+}
