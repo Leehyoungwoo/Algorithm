a34827ba169a0fc41665df32847d1c048f0ba4ad
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-24 20:03:44 +0900

    solution: 2022년 카카오 인턴 채용 3번
    dp 문제
    범위가 크지 않기 때문에 3중 포문으로 해결이 가능한 문제
    dp[알고][코딩] 배열로 현재 위치에서 + 1씩 공부하는 것과 문제를 풀어서 올리는 것중에 최소 시간이 드는 값을 찾아서 디피 배열을 채우고
    
    모든 문제를 풀 수 있는 상태인지 확인하여 그중 최소값을 리턴해주면 되는 심플한 문제
    
    아이디어 생각하기가 쉽지 않았지만 생각하고 나니 함정도 없고 간단해보이는 문제

---
+++ src/forkakao/blindrecruitment2023/떨어뜨리기.java
@@ -0,0 +1,30 @@
+package forkakao.blindrecruitment2023;
+
+import java.util.*;
+
+public class 떨어뜨리기 {
+
+    public static void main(String[] args) {
+        Solution7 solution = new Solution7();
+        int[][] edges = {
+                {2, 4},
+                {1, 2},
+                {6, 8},
+                {1, 3},
+                {5, 7},
+                {2, 5},
+                {3, 6},
+                {6, 10},
+                {6, 9}
+        };
+        int[] target = {0, 0, 0, 3, 0, 0, 5, 1, 2, 3};
+        int[] answer = solution.solution(edges, target);
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+}
+class Solution7 {
+    public int[] solution(int[][] edges, int[] target) {
+        int[] answer = {};
+        return answer;
+    }
+}
