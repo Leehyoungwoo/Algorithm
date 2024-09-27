a162d9a96e287fff7d3dcfaf494fee49315a6127
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-27 19:37:13 +0900

    solution: 2022년 카카오 블라인드 채용 2번
    역시 구현 문제
    
    요구사항에 맞춰서 구현하면 되는 문제.
    처음에 들어갔다가 나갈떄마다 요금을 계산하는줄 알았다가 틀렸음. 시간을 한번에 계산해서 요금을 계산한다는걸 주의깊게 봤어야 함
    주의 필요
    요금 계산은 방법없이 메서드로 구현하고
    차 별로 in,out 상태 관리 신경쓰고 누적 시간을 잘 계산하면 쉽게 풀릴 문제
    answer 계산은 set으로 받아준 후 list로 바꿔주면 쉽게 처리 가능
     import java.time.*; 덕에 시간 처리를 쉽게 한 감이 있지만, 실제 구현했다고 하더라도 크게 어렵지는 않았을거 같음
     LocalTime startTime = LocalTime.of(Integer.parseInt(inTime[0]), Integer.parseInt(inTime[1]));과
     long parkingTime = Duration.between(startTime, endTime).toMinutes();를 알아두면 좋을거 같음

---
+++ src/forkakao/blindrecrument2022/주차_요금_계산.java
@@ -0,0 +1,102 @@
+package forkakao.blindrecrument2022;
+
+import java.util.*;
+import java.time.*;
+
+public class 주차_요금_계산 {
+
+    public static void main(String[] args) {
+        Solution3 solution = new Solution3();
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+        int[] answer = solution.solution(fees, records);
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+}
+
+class Solution3 {
+
+    Set<String> cars = new HashSet<>();
+    Map<String, String> carStatus = new HashMap<>();
+    Map<String, Long> carPerTime = new HashMap<>();
+    private final String[] outTime = {"23", "59"};
+
+    public int[] solution(int[] fees, String[] records) {
+        int[] answer = {};
+
+        for (int i = 0; i < records.length; i++) {
+            String[] s = records[i].split(" ");
+            String time = s[0];
+            String car = s[1];
+            String type = s[2];
+            // 마지막에 차넘버로 정렬하려고 만든 set임
+            cars.add(car);
+            // type이 in이면 status에 시간 보관해주고 out이면 기존 시간 빼와서 아웃 시간이랑 비교해서 요금 산출
+            if (type.equals("IN")) {
+                carStatus.put(car, time);
+                continue;
+            }
+
+            // 요금 처리
+            if (type.equals("OUT")) {
+                String in = carStatus.get(car);
+                String[] inTime = in.split(":");
+                LocalTime startTime = LocalTime.of(Integer.parseInt(inTime[0]), Integer.parseInt(inTime[1]));
+                String[] outTime = time.split(":");
+                LocalTime endTime = LocalTime.of(Integer.parseInt(outTime[0]), Integer.parseInt(outTime[1]));
+                long parkingTime = Duration.between(startTime, endTime).toMinutes();
+                carPerTime.put(car, carPerTime.getOrDefault(car, 0L) + parkingTime);
+                carStatus.put(car, null);
+            }
+        }
+        // in만 있는 차량 요금 처리
+        for (Map.Entry<String, String> entry : carStatus.entrySet()) {
+            if (!Objects.isNull(entry.getValue())) {
+                String car = entry.getKey();
+                String in = carStatus.get(car);
+                String[] inTime = in.split(":");
+                LocalTime startTime = LocalTime.of(Integer.parseInt(inTime[0]), Integer.parseInt(inTime[1]));
+                LocalTime endTime = LocalTime.of(Integer.parseInt(outTime[0]), Integer.parseInt(outTime[1]));
+                long parkingTime = Duration.between(startTime, endTime).toMinutes();
+                carPerTime.put(car, carPerTime.getOrDefault(car, 0L) + parkingTime);
+                carStatus.put(car, null);
+            }
+        }
+
+        List<String> carList = new ArrayList<>(cars);
+        Collections.sort(carList);
+        answer = new int[carList.size()];
+        for (int i = 0; i < carList.size(); i++) {
+            String car = carList.get(i);
+            long time = carPerTime.get(car);
+            answer[i] = this.caculateFee(time, fees);
+        }
+
+
+        return answer;
+    }
+
+    private int caculateFee(long parkingTime, int[] fees) {
+        int basicTime = fees[0];
+        int basicPrice = fees[1];
+        int addTime = fees[2];
+        int addPrice = fees[3];
+
+        if (parkingTime <= basicTime) {
+            return basicPrice;
+        }
+
+        int extraPrice = 0;
+        if (parkingTime > basicTime) {
+            int extraTime = (int) (parkingTime - basicTime);
+            if (extraTime % addTime == 0) {
+                extraPrice = extraTime / addTime * addPrice;
+            } else {
+                extraPrice = (extraTime / addTime + 1) * addPrice;
+            }
+
+        }
+
+        return basicPrice + extraPrice;
+    }
+}
