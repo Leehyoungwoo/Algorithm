d5da007893635335a11cd400cd4f963077ce0f31
dlguddn3024 <dlguddn3024@gmail.com>
2024-09-27 20:33:03 +0900

    solution: 2022년 카카오 블라인드 채용 2번
    백트래킹 문제(아마 다른 방법이 있을지도?)
    n은 10까지이고 배열 길이도 11이라 시간이 터질리는 없다고 생각
    요구사항에 맞게 구현하면 됨
    라이언을 이기게 만들려면 화살이 어피치가 쏜거보다 더 남았을때, 해당 점수를 한발만 더 쏘면 되고 안되면 백트래킹해서 다음 점수 노리기
    종료 조건에도 백트래킹을 넣어줘야 하는데 다른 탐색을 할 때, lionInfo가 원상태여야 하니까
    처음에 라이언이 절대 못이기는 예외 케이스가 어피치가 모든 화살을 10점에 맞추는 것이라 생각했는데 오히려 그러면 9, 8, 7, 6점 획득해서 이겨버릴 수 있었음
    그냥 maxDiff를 모든 경우에서 계산하고 0보다 작거나 같으면 절대 못이기는 처리를 하는게 맞는 방법이었음

---
+++ src/forkakao/blindrecrument2022/양궁대회.java
@@ -0,0 +1,86 @@
+package forkakao.blindrecrument2022;
+
+import java.util.*;
+
+public class 양궁대회 {
+
+    public static void main(String[] args) {
+        Solution4 solution = new Solution4();
+        int[] answer = solution.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0});
+        Arrays.stream(answer).forEach(System.out::println);
+    }
+}
+
+class Solution4 {
+
+    private int maxDiff = 0;
+    private int[] bestShot;
+
+    public int[] solution(int n, int[] info) {
+        bestShot = new int[11];
+
+        backTracking(n, 0, new int[11], info);
+        // 라이언이 절대 어피치를 이길 수 없는 경우
+        // 어피치가 전부 베스트 샷으로 맞춘게 아니더라도 절대 이길 수 없을 수 있음
+        // n = 5, info = [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]이면 무슨 짓을 해도 못이김
+        if (maxDiff == 0) {
+            return new int[]{-1};
+        }
+        return bestShot;
+    }
+
+    private void backTracking(int leftArrow, int idx, int[] lionInfo, int[] apeachInfo) {
+        // 남은 화살이 없거나 끝까지 다 쐈으면 종료
+        if (leftArrow == 0 || idx == 11) {
+            // 만약 끝까지 쐈는데 화살이 남았으면 0점에 다 쏴버리기
+            lionInfo[10] += leftArrow;
+            compareScore(lionInfo, apeachInfo);
+            // 되돌리기
+            lionInfo[10] -= leftArrow;
+            return;
+        }
+
+        if (leftArrow > apeachInfo[idx]) {
+            // 한발 더 쏴서 해당 점수 가져가기
+            lionInfo[idx] = apeachInfo[idx] + 1;
+            backTracking(leftArrow - lionInfo[idx], idx + 1, lionInfo, apeachInfo);
+            // 백트래킹
+            lionInfo[idx] = 0;
+        }
+
+        // 남은 화살이 apeach의 해당 점수를 못이기면 다음 점수를 노려야하니까
+        backTracking(leftArrow, idx + 1, lionInfo, apeachInfo);
+    }
+
+    private void compareScore(int[] lionInfo, int[] apeachInfo) {
+        int lionScore = 0;
+        int apeachScore = 0;
+
+        for (int i = 0; i < 11; i++) {
+            if (lionInfo[i] > apeachInfo[i]) {
+                lionScore += 10 - i;
+                // 어피치가 0이상인데 라이인이 어피치보다 크지 않으면 어피치 점수 get
+            } else if (apeachInfo[i] > 0) {
+                apeachScore += 10 - i;
+            }
+        }
+
+        int diff = lionScore - apeachScore;
+        if (diff > maxDiff || diff == maxDiff && isBetter(lionInfo)) {
+            maxDiff = diff;
+            bestShot = lionInfo.clone();
+        }
+    }
+
+    private boolean isBetter(int[] lionInfo) {
+        for (int i = 10; i >= 0; i--) {
+            if (bestShot[i] < lionInfo[i]) {
+                return true;
+            } else if (bestShot[i] > lionInfo[i]) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
