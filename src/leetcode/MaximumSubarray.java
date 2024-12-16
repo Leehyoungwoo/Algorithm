041c3e6ade3a0858b97eeefaa695995213e58f5b
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-16 21:21:10 +0900

    solution: 카데인 알고리즘 1. 배열의 각 원소를 차례로 탐색하며, 현재까지의 최대 누적 합을 계산 2. 만약 현재까지의 누적 합(curSum)이 음수가 된다면, 이는 이후에 나올 값에 더해도 최대 합을 증가시키지 못하므로 누적 합을 0으로 초기화하고 새로운 시작으로 간주(초기화 다시 시작하는게 더 크니까) 3. 매번 계산된 누적 합 중에서 최댓값(maxSum)을 갱신하여 저장

---
+++ src/leetcode/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        System.out.println(answer);
+    }
+
+    static class Solution {
+        public int maxSubArray(int[] nums) {
+            int maxSum = nums[0];
+            int curSum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                curSum += nums[i];
+                maxSum = Math.max(curSum, maxSum);
+
+                if (curSum < 0) {
+                    curSum = 0;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
