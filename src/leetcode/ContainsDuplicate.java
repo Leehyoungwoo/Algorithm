99d5f5ee6ccdfd3505449fadb70e9b3019069cc5
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-16 17:48:57 +0900

    solution: 자료구조 문제 Map으로 nums안의 원소를 키로 등장 횟수를 value로 기록하고 순회했을 떄 value가 1 초과인 key가 있으면 true 반환

---
+++ src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean answer = solution.containsDuplicate(new int[]{1, 2, 3, 1});
+        System.out.println(answer);
+    }
+
+    static class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > 1) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
