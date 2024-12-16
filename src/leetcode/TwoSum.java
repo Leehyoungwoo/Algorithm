e9df1c8bf07059d13a35c2d2979406f53b23444f
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-16 17:13:53 +0900

    solution: map에 nums[i], i를 넣고 순회를 돌면서 target - nums[i]가 key로 있으면 return하게 코드를 수정하여 O(n^2)에서 O(n)으로 시간 복잡도 개선 가능

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -14,15 +16,18 @@ public class TwoSum {
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] result = {};
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    result = new int[]{i, j};
-                    break;
-                }
+            int cur = nums[i];
+            // cur + x = target
+            // x = target - cur
+            int x = target - cur;
+            if (map.containsKey(x)) {
+                return new int[] {map.get(x), i};
             }
+            map.put(cur, i);
         }
-        return result;
+
+        return null;
     }
 }
