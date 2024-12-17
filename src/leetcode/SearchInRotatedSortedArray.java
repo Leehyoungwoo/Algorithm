f0e227a431109b2efeed96c4c3ca035c5f1a36dc
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-17 17:31:20 +0900

    solution: 1. Map에 각 값을 키로 인덱스를 넣어서 탐색하면 시간복잡도 O(1)로 탐색 가능 2. O(logN)을 맞추기 위해 정렬후 이분 탐색으로 통해 인덱스를 구할 수 있음

---
+++ src/leetcode/SearchInRotatedSortedArray.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+
+    static class Solution {
+        public int search(int[] nums, int target) {
+//            Map<Integer, Integer> map = new HashMap<>();
+//            for (int i = 0; i < nums.length; i++) {
+//                map.put(nums[i], i);
+//            }
+//            return map.getOrDefault(target, -1);
+            int[] copyNum = Arrays.copyOf(nums, nums.length);
+            Arrays.sort(copyNum);
+            int left = 0;
+            int right = nums.length - 1;
+            int answer = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (copyNum[mid] < target) {
+                    left = mid + 1;
+                }
+                if (copyNum[mid] > target) {
+                    right = mid - 1;
+                }
+                if (copyNum[mid] == target) {
+                    int idx = findIdx(target, nums);
+                    return idx;
+                }
+            }
+            return answer;
+        }
+
+        private int findIdx(int target, int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                if (target == nums[i]) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+    }
+}
