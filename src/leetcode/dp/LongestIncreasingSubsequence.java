0e643215d505cf7a33ccd43016b35a0eb3e0d4ec
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-18 19:56:34 +0900

    Solution : DP문제 이전 원소들과 비교하여 증가하는 부분 수열의 최대 길이를 계산하는 동적 계획법 적용

---
+++ src/leetcode/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+package leetcode.dp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLIS(new int[] {10,9,2,5,3,7,101,18}));
+    }
+
+    static class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp, 1);
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            return Arrays.stream(dp)
+                    .max()
+                    .getAsInt();
+        }
+    }
+}
