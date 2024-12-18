58747d793991db9886296230804bc2218dcd8cc6
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-18 15:56:44 +0900

    Solution: 1. Integer.toBinaryString으로 스트링 변환(이때 32비트를 유지하도록 String.format("%32s")활용하고 빈자리를 '0'으로 replace한 후 스트링빌더의 리버스 함수로 뒤집고 마지막에 변환 2. result에 reverse를 붙이는데 result를 오른쪽으로 밀고 n의 끝자리를 붙이고 n을 왼쪽으로 밀기 반복

---
+++ src/leetcode/ReverseBits.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class ReverseBits {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseBits(00000010100101000001111010011100));
+    }
+
+    static class Solution {
+        public int reverseBits(int n) {
+            // String으로 변경 후 풀이
+//            String binaryString = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+//
+//            String reversedBinaryString = new StringBuilder(binaryString).reverse().toString();
+//
+//            return (int) Long.parseLong(reversedBinaryString, 2);
+            // 비트 연산 풀이
+            int result = 0;
+
+            for (int i = 0; i <= 31; i++) {
+                result = result << 1 | (n & 1);
+
+                n >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
