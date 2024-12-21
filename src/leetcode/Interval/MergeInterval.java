a8f642ec432564e90025a1266a139d363a067a56
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-21 17:09:14 +0900

    Solution : - 주어진 구간들을 오름차순으로 정렬한 후, 하나씩 비교하며 구간을 합침 - 구간이 겹치지 않으면 결과 리스트에 추가하고, 겹친다면 끝값을 갱신하여 하나의 구간으로 병합 - 구간 병합을 완료한 후, 리스트를 2차원 배열로 변환하여 반환 - 배열의 크기가 커지거나 구간이 많을 때에도 효율적으로 동작하도록 설계

---
+++ src/leetcode/Interval/MergeInterval.java
@@ -0,0 +1,62 @@
+package leetcode.Interval;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeInterval {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.merge(new int[][]{
+                {0, 0},
+                {1, 2},
+                {5, 5},
+                {2, 4},
+                {3, 3},
+                {5, 6},
+                {5, 6},
+                {4, 6},
+                {0, 0},
+                {1, 2},
+                {0, 2},
+                {4, 5}
+        }));
+    }
+
+    static class Solution {
+
+        private final int max = 10_000;
+
+        public int[][] merge(int[][] intervals) {
+            Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+            List<List<Integer>> res = new ArrayList<>();
+            int start = intervals[0][0];
+            int end = intervals[0][1];;
+            for (int[] interval : intervals) {
+
+                if (end < interval[0]) {
+                    res.add(new ArrayList<>(Arrays.asList(start, end)));
+                    start = interval[0];
+                    end = interval[1];
+                    continue;
+                }
+
+                if (start <= interval[0] && end >= interval[1]) {
+                    continue;
+                }
+
+                if (start <= interval[0] && end <= interval[1]) {
+                    end = interval[1];
+                }
+            }
+            res.add(new ArrayList<>(Arrays.asList(start, end)));
+
+            return res.stream().map(list -> list.stream()
+                            .mapToInt(Integer::intValue)
+                            .toArray())
+                    .toArray(int[][]::new);
+        }
+    }
+}
