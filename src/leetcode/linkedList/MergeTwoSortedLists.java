7ce8fc130522718e85ac0b0e26686123089a3886
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-24 21:08:36 +0900

    Solution : [풀이] k개의 정렬된 연결 리스트를 하나로 합치기
    - 각 연결 리스트의 값을 하나의 ArrayList에 저장
    - 저장한 값들을 정렬하여 하나의 연결 리스트로 재구성
    - 새로 만든 연결 리스트의 첫 번째 노드를 head로 설정하고, 이후 노드를 연결하면서 순차적으로 리스트 생성
    - 예외 처리: lists가 null이거나 길이가 0인 경우 null 반환
    - ListNode를 사용하여 새로운 연결 리스트 구성

--- src/leetcode/linkedList/MergeTwoSortedLists.java
+++ src/leetcode/linkedList/MergeTwoSortedLists.java
@@ -1,63 +1,63 @@
-package leetcode.linkedList;
-
-
-public class MergeTwoSortedLists {
-
-    public static void main(String[] args) {
-
-    }
-
-    static class Solution {
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            if (list1 == null) return list2;
-            if (list2 == null) return list1;
-
-            ListNode head = list1.val <= list2.val ? list1 : list2;
-            ListNode cur = head;
-
-            if (head == list1) {
-                list1 = list1.next;
-            } else {
-                list2 = list2.next;
-            }
-
-            while (list1 != null && list2 != null) {
-                if (list1.val <= list2.val) {
-                    cur.next = list1;
-                    list1 = list1.next;
-                } else {
-                    cur.next = list2;
-                    list2 = list2.next;
-                }
-
-                cur = cur.next;
-            }
-
-            if (list1 != null) {
-                cur.next = list1;
-            } else {
-                cur.next = list2;
-            }
-
-            return head;
-        }
-    }
-
-    static class ListNode {
-        int val;
-        RemoveNthNodeFromEndOfList.ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, RemoveNthNodeFromEndOfList.ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-}
+//package leetcode.linkedList;
+//
+//
+//public class MergeTwoSortedLists {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    static class Solution {
+//        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//            if (list1 == null) return list2;
+//            if (list2 == null) return list1;
+//
+//            ListNode head = list1.val <= list2.val ? list1 : list2;
+//            ListNode cur = head;
+//
+//            if (head == list1) {
+//                list1 = list1.next;
+//            } else {
+//                list2 = list2.next;
+//            }
+//
+//            while (list1 != null && list2 != null) {
+//                if (list1.val <= list2.val) {
+//                    cur.next = list1;
+//                    list1 = list1.next;
+//                } else {
+//                    cur.next = list2;
+//                    list2 = list2.next;
+//                }
+//
+//                cur = cur.next;
+//            }
+//
+//            if (list1 != null) {
+//                cur.next = list1;
+//            } else {
+//                cur.next = list2;
+//            }
+//
+//            return head;
+//        }
+//    }
+//
+//     static class ListNode {
+//        int val;
+//        RemoveNthNodeFromEndOfList.ListNode next;
+//
+//        ListNode() {
+//        }
+//
+//        ListNode(int val) {
+//            this.val = val;
+//        }
+//
+//        ListNode(int val, RemoveNthNodeFromEndOfList.ListNode next) {
+//            this.val = val;
+//            this.next = next;
+//        }
+//    }
+//
+//}
