256f7eb5d182703e10b299c3cf6cd96de0a70b63
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-25 17:43:57 +0900

    Solution : "주어진 문자열에서 가장 긴 회문 부분 문자열을 구하는 문제 해결
    문자열의 모든 부분 문자열을 탐색하여 회문 여부를 확인
    회문을 확인하는 isValid 메서드로 각 부분 문자열이 회문인지 검사
    가장 긴 회문을 maxLen 변수에 저장하고, 그 결과를 res에 업데이트하여 반환"

---
+++ src/leetcode/string/LongestPalindromicSubstring.java
@@ -0,0 +1,42 @@
+package leetcode.string;
+
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("bb"));
+    }
+
+    static class Solution {
+        public String longestPalindrome(String s) {
+            if (s.length() == 1) {
+                return s;
+            }
+            String res = "";
+            int maxLen = Integer.MIN_VALUE;
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i + 1; j <= s.length(); j++) {
+                    String target = s.substring(i, j);
+                    if (isValid(target)) {
+                        if (maxLen < target.length()) {
+                            maxLen = target.length();
+                            res = target;
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private boolean isValid(String target) {
+            for (int i = 0; i < target.length() / 2; i++) {
+                if (target.charAt(i) != target.charAt(target.length() - 1 - i)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
