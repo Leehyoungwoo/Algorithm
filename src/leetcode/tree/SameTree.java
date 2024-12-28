46157857caffd438f505518f656dc68039243e83
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-28 16:31:25 +0900

    Solution : 두 이진 트리의 동일성을 확인하는 알고리즘 구현
    - 두 트리가 모두 null인 경우 true 반환
    - 한쪽만 null인 경우 false 반환
    - 현재 노드의 값이 같고, 왼쪽 및 오른쪽 하위 트리가 모두 동일한 경우 true 반환
    - 재귀적으로 왼쪽 및 오른쪽 하위 트리를 비교하여 동일성을 확인

--- src/leetcode/tree/SameTree.java
+++ src/leetcode/tree/SameTree.java
@@ -36,7 +36,7 @@ public class SameTree {
             this.val = val;
         }
 
-        TreeNode(int val, MaximumDepthOfBinaryTree.TreeNode left, MaximumDepthOfBinaryTree.TreeNode right) {
+        TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
