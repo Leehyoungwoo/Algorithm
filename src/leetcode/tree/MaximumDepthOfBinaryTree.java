3386dfa3d39e6a340e34685d1deada2b8ebbc377
dlguddn3024 <dlguddn3024@gmail.com>
2024-12-28 16:20:15 +0900

    Solution : 이진 트리의 최대 깊이 계산 구현
    - 재귀적 접근을 사용하여 문제 해결
    - `TreeNode`가 null일 경우 0 반환
    - 왼쪽 및 오른쪽 하위 트리의 최대 깊이를 각각 구한 뒤, 더 큰 값을 선택하고 1을 더하여 루트 노드까지의 깊이를 계산
    - 시간 복잡도: O(n) (트리의 모든 노드를 한 번씩 방문)
    - 공간 복잡도: O(h) (재귀 호출 스택의 깊이, h는 트리의 높이)

---
+++ src/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,36 @@
+package leetcode.tree;
+
+import javax.swing.tree.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
